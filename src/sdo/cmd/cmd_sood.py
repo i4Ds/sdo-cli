3d53c9a96eaa24e971d2309098a2e86b19c8bb11
Marius Giger <marius@akenza.io>
2022-07-31 14:42:47 +0200

    Adds a threshold based baseline model

--- src/sdo/cmd/cmd_sood.py
+++ src/sdo/cmd/cmd_sood.py
@@ -1,7 +1,6 @@
 import click
 
-from sdo.cmd.sood import cmd_ce_vae
-from sdo.cmd.sood import cmd_ae
+from sdo.cmd.sood import cmd_ce_vae, cmd_ae, cmd_threshold
 
 
 @click.group("sood")
@@ -11,3 +10,4 @@ def cli():
 
 cli.add_command(cmd_ce_vae.cli)
 cli.add_command(cmd_ae.cli)
+cli.add_command(cmd_threshold.cli)
